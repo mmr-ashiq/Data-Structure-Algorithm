c2bdc35f78e46c9c4aff1378be44dea2fe997882
MD. Mizanur Rahman Ashiq <30866225+mmr-ashiq@users.noreply.github.com>
2020-01-13 13:09:26 +0600

    Create Insert an element at any given index.c
    c

---
+++ Array/Insert an element at any given index.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main()
+{
+    int a[10],n,i,new_value,new_index;
+    printf("How many numbers you wanna add? ");
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+        scanf("%d",&a[i]);
+    for(i=0;i<n;i++)
+        printf("%d ",a[i]);
+    printf("\nEnter the value: ");
+    scanf("%d",&new_value);
+    printf("which index you wanna add? ");
+    scanf("%d",&new_index);
+    a[new_index]=new_value;
+    for(i=n-1;i>=new_index;i--)
+        a[i+1]=a[i];
+    for(i=0;i<new_value;i++)
+        printf("%d ",a[i]);
+        
+    return 0;
+}
