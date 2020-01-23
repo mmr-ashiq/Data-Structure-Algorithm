771d2b14d3d121f00eeee6c264816692278ed68b
MD. Mizanur Rahman Ashiq <30866225+mmr-ashiq@users.noreply.github.com>
2020-01-23 07:16:39 +0600

    Update delete an element.c

--- Array/delete an element.c
+++ Array/delete an element.c
@@ -1,16 +1,28 @@
 #include <stdio.h>
 
-int main(void) 
+int main()
 {
-  int n,i,pos,a[10];
-  scanf("%d",&n);
-  for(i=0;i<n;i++)
-    scanf("%d",&a[i]);
-  printf("which index you want to delete: ");
-  scanf("%d",&pos);
-  for(i=pos;i<n;i++)
-    a[i]=a[i+1];
-  for(i=0;i<n-1;i++)
-    printf("%d ",a[i]);
-  return 0;
+
+      int n,i,pos,a[10];
+      printf("how many element you want to store: ");
+      scanf("%d",&n);
+      printf("Enter these element: ");
+      for(i=0;i<n;i++)
+
+        scanf("%d",&a[i]);
+
+      printf("which index you want to Delete: ");
+
+      scanf("%d",&pos);
+
+      for(i=pos;i<n;i++)
+
+        a[i]=a[i+1];
+
+      for(i=0;i<n-1;i++)
+
+        printf("%d ",a[i]);
+
+      return 0;
+
 }
