c8b6212c1072d935e1ae1a0217ada10c3490f3ef
MD. Mizanur Rahman Ashiq <30866225+mmr-ashiq@users.noreply.github.com>
2020-01-31 12:41:46 +0600

    Update recursionbinarysearch.c

--- Array/recursionbinarysearch.c
+++ Array/recursionbinarysearch.c
@@ -1,61 +1,60 @@
-//this code from a udemy course
 #include<stdio.h>
-struct Array
-{
-    int A[10];
-    int size;
-    int length;
-};
-void Display(struct Array arr)
-{
-    int i;
-    printf("\nElements are\n");
-    for(i=0; i<arr.length; i++)
-        printf("%d ",arr.A[i]);
-}
-void swap(int *x,int *y)
-{
-    int temp=*x;
-    *x=*y;
-    *y=temp;
-}
-int BinarySearch(struct Array arr,int key)
-{
-    int l,mid,h;
-    l=0;
-    h=arr.length-1;
-    while(l<=h)
-    {
-        mid=(l+h)/2;
-        if(key==arr.A[mid])
-            return mid;
-        else if(key<arr.A[mid])
-            h=mid-1;
-        else
-            l=mid+1;
-    }
-    return -1;
-}
-int RBinSearch(int a[],int l,int h,int key)
-{
-    int mid=0;
-    if(l<=h)
-    {
-
-        mid=(l+h)/2;
-        if(key==a[mid])
-            return mid;
-        else if(key<a[mid])
-            return RBinSearch(a,l,mid-1,key);
-    }
-    else
-        return RBinSearch(a,mid+1,h,key);
-    return -1;
-}
 int main()
 {
-    struct Array arr= {{2,3,9,16,18,21,28,32,35},10,9};
-    printf("%d\n",RBinSearch(arr.A,0,arr.length,9));
-    Display(arr);
+    int n,a[20],i,new_index=-1,s;
+    printf("how many element you want to add in array: ");
+    scanf("%d",&n);
+    printf("Enter elements: ");
+    for(i=0;i<n;i++)
+        scanf("%d",&a[i]);
+    printf("array looks like now: ");
+    for(i=0;i<n;i++)
+        printf("%d ",a[i]);
+    printf("\nEnter an element for searching: ");
+    scanf("%d",&s);
+
+    for(i=0;i<n;i++)
+    {
+        //2 4 6 8 10 12 15 ; S=4
+        if(a[i]==s)
+        {
+            new_index=i;
+            for(i=new_index+2;i<n-1;i++)
+            {
+                a[i]=a[i+1];
+            }
+            for(i=0;i<n-2;i++)
+                printf("%d ",a[i]);
+        }
+
+        //2 4 6 8 10 12 15 ; S=10
+        else if(a[n-3]==s)
+        {
+            new_index=a[n-3];
+            for(i=0;i<n-1;i++)
+                printf("%d ",a[i]);
+        }
+
+        //2 4 6 8 10 12 15 ; S=15
+        else if(a[n-1]==s)
+        {
+            new_index=a[n-1];
+            for(i=0;i<n-1;i++)
+                printf("%d ",a[i]);
+            printf(",No Double Next Found!");
+        }
+
+        //2 4 6 8 10 12 15 ; S=12
+        else if(a[n-2]==s)
+        {
+            new_index=a[n-2];
+            for(i=0;i<n-1;i++)
+                printf("%d ",a[i]);
+            printf(",No Double Next Found!");
+        }
+    }
+    if(new_index==-1)
+        printf(",%d doesn't exist in this array!",s);
+
     return 0;
 }
