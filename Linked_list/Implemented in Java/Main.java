ef53cbdbbb9aa7ca462eb04956bef1c7003fc8b4
Md. Mizanur Rahman Ashiq <30866225+mmr-ashiq@users.noreply.github.com>
2020-05-13 11:38:02 +0600

    Update and rename Main.java to basic.java
    create linked list and how a linked list word.

--- Linked_list/Implemented in Java/Main.java
+++
@@ -1,1 +0,0 @@
-ekhon e asbe
---
+++ Linked_list/Implemented in Java/basic.java
@@ -0,0 +1,20 @@
+package com.mmrproduct;
+
+import java.util.LinkedList;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        LinkedList<String> countryNames = new LinkedList<String>();
+
+        countryNames.add("Bangladesh");
+        countryNames.add("America");
+        countryNames.add("Canada");
+        countryNames.add("Germany");
+        countryNames.add("London");
+
+        System.out.println(countryNames);
+
+    }
+}
