c2103d1d0c431554b3ee6f461b44bf6db393ab20
Md. Mizanur Rahman Ashiq <30866225+mmr-ashiq@users.noreply.github.com>
2020-06-08 10:56:16 +0600

    Update Merge array.c

--- 1.Array/Implemented in C/Merge array.c
+++ 1.Array/Implemented in C/Merge array.c
@@ -18,8 +18,7 @@ struct Array* Merge(struct Array *arr1,struct Array *arr2)
 {
     int i,j,k;
     i=j=k=0;
-    struct Array *arr3=(struct Array *)malloc(sizeof(struct
-                       Array));
+    struct Array *arr3=(struct Array *)malloc(sizeof(struct Array));
     while(i<arr1->length && j<arr2->length)
     {
         if(arr1->A[i]<arr2->A[j])
