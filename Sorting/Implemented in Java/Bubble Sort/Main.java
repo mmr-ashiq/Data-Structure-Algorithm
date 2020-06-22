33090ca6018c600726b77e4ecfc486590a18d940
Md. Mizanur Rahman Ashiq <30866225+mmr-ashiq@users.noreply.github.com>
2020-06-22 22:05:35 +0600

    Update and rename Sorting/Implemented in Java/Main.java to Sorting/Implemented in Java/Bubble Sort/Main.java

---
+++ Sorting/Implemented in Java/Bubble Sort/Main.java
@@ -0,0 +1,17 @@
+package com.company;
+
+import java.util.Arrays;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        int [] unsortedArray = {6,9,8,2,3,1,10,7,5};
+        var giveMeSortedArray = new BubbleSort();
+        giveMeSortedArray.sort(unsortedArray);
+        System.out.println(Arrays.toString(unsortedArray));
+
+    }
+
+    //Time complexity of Bubble Sort : O(N^2)
+}
